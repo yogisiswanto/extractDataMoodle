65d753b34306b38810ecc5922825f9298dea9e40
yogi_siswanto <yogisiswanto.c2@gmail.com>
2019-12-03 13:59:21 +0700

    Adding Tab Panel

--- Status2.php
+++ Status2.php
@@ -12,31 +12,97 @@
     <title>Hello, world!</title>
   </head>
   <body>
+
   <div class="container-fluid">
   <!-- Content here -->
     
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="Utama2.php" style="font-size:1.2rem">Utama</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true" style="font-size:1.2rem">|</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="Status.php" style="font-size:1.2rem">Status<span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true" style="font-size:1.2rem">|</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#" style="font-size:1.2rem">Kolaborasi<span class="sr-only">(current)</span></a>
-                </li>
                 
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="Utama2.php" style="font-size:1.2rem">Utama</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true" style="font-size:1.2rem">|</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="Status.php" style="font-size:1.2rem">Status<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true" style="font-size:1.2rem">|</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" style="font-size:1.2rem">Kolaborasi<span class="sr-only">(current)</span></a>
+                    </li>
+                
+                <!-- End of navbar-nav -->
                 </ul>
+
+            <!-- End of NavbarNav -->
             </div>
+        
+        <!-- End of Navbar Menu -->
         </nav>
+        
+        <div class="row">
+
+            <div class="col">
+                <br>
+            </div>
+
+        <!-- End of Row -->
+        </div>
+
+        <div class="row">
+
+            <div class="col-sm-3">
+            
+            </div>
+            <div class="col-6">
+
+                <div class="card border-primary mb-3">
+                    <div class="card-header">
+                    
+                        <ul class="nav nav-tabs" id="myTab" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+                            </li>
+
+                        <!-- End of myTab -->
+                        </ul>
+
+                    <!-- End of card-header -->
+                    </div>
+                    <div class="card-body">
+                        
+                        <div class="tab-content" id="myTabContent">
+                            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">Home</div>
+                            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">Profile</div>
+                            <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">Contact</div>
+
+                        <!-- End of tab-content -->
+                        </div>                    
+
+                    <!-- End of card-body -->
+                    </div>
+
+                <!-- End of card -->
+                </div>
+
+            <!-- End of col-6 -->
+            </div>
+                   
+        <!-- End of Row -->
+        </div>
+
+    <!-- End of Container -->
     </div>
 
     <!-- Optional JavaScript -->
